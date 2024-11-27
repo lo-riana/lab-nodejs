e2f4bef02ed772c8ac09577a7be203e594de5119
lo-riana <151836369+lo-riana@users.noreply.github.com>
2024-11-27 11:59:36 -0500

    Add file task.js

---
+++ task.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const mysql = require('mysql2');
+
+const app = express();
+app.use(express.json());
+
+const dbCon = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "fytiafytia22$",
+    database: "my_database",
+    port: 3306
+});
+
+dbCon.connect((err) => {
+    if (err) throw err;
+    console.log("Successfully connected to MySQL!");
+});
+
+app.post('/add', (req, res) => {
+    const { name, email } = req.body;
+    const sql = `INSERT INTO users (name, email) VALUES (?, ?)`;
+    dbCon.query(sql, [name, email], (err, result) => {
+        if (err) throw err;
+        res.send('User added successfully!');
+    });
+});
+
+app.get('/records', (req, res) => {
+    const sql = `SELECT * FROM users`;
+    dbCon.query(sql, (err, results) => {
+        if (err) throw err;
+        res.json(results);
+    });
+});
+
+app.get('/record/:id', (req, res) => {
+    const sql = `SELECT * FROM users WHERE id = ?`;
+    dbCon.query(sql, [req.params.id], (err, result) => {
+        if (err) throw err;
+        res.json(result);
+    });
+});
+
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}/add`);
+});
